f9f3e920fb8e6cef17472a8937fdd9978e713fa1
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2022-10-20 22:03:39 -0300

    Set global styles

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback } from "react";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import * as SplashScreen from "expo-splash-screen";
+import theme from "./src/styles/theme";
+
+import { Home } from "./src/screens/Home";
+import { ThemeProvider } from "styled-components";
+
+export default function App() {
+  const [loadedFont] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (loadedFont) {
+    SplashScreen.hideAsync();
+  }
+  if (!loadedFont) return null;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Container, Title } from "./styles";
+
+interface HomeProps {}
+
+export const Home = (props: HomeProps) => {
+  return (
+    <Container>
+      <Title>HOME</Title>
+    </Container>
+  );
+};
