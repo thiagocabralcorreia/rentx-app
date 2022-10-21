a9bde430e63cc05972ee94a0313145874683e66a
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2022-10-20 22:51:23 -0300

    Add home screen header and assets

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,25 @@
 import * as React from "react";
-import { Container, Title } from "./styles";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from "../../assets/logo.svg";
+
+import { Container, Header, TotalCars } from "./styles";
 
 interface HomeProps {}
 
 export const Home = (props: HomeProps) => {
   return (
     <Container>
-      <Title>HOME</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <Logo width={RFValue(114)} height={RFValue(11)} />
+        <TotalCars>Total de 12 carros</TotalCars>
+      </Header>
     </Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,12 +1,29 @@
-import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+  ${({ theme }) => css`
+    flex: 1;
+    background-color: ${theme.colors.background_primary};
+  `}
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
-  font-family: ${({ theme }) => theme.fonts.secondary_600};
+export const Header = styled.View`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 113px;
+    padding: 42px 24px 0 24px;
+    background-color: ${theme.colors.header};
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  `}
+`;
+
+export const TotalCars = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${RFValue(12)}px;
+    font-family: ${theme.fonts.primary_400};
+    color: ${theme.colors.text};
+  `}
 `;
